a5b38e5dc28b43207d6d928d09fbc758d5830c1b
Noah Coffey <noah.coffey@gmail.com>
2014-03-21 12:11:01 -0400

    Make script run every 10 seconds.

--- _fetchScore.php
+++ _fetchScore.php
@@ -1,20 +1,35 @@
 <?php
 
-require_once 'unirest-php/lib/Unirest.php';
 
-$file = 'score.json';
+updateScore(0);
+updateScore(10);
+updateScore(20);
+updateScore(30);
+updateScore(40);
+updateScore(50);
 
-$response = Unirest::get("https://indianapolis.bracketsforgood.org/?citimark-division");
 
-//PIH
-$res = explode('p2_entry_id":158,"p2_score":"', $response->body);
-$res = explode('","', $res[1]);
-$score['pih'] = number_format($res[0]);
+function updateScore($sleep) {
+  
+  sleep($sleep);
+  
+  require_once 'unirest-php/lib/Unirest.php';
 
-//Other
-$res = explode('p1_entry_id":154,"p1_score":"', $response->body);
-$res = explode('","', $res[1]);
-$score['other'] = number_format($res[0]);
-
-file_put_contents($file, json_encode($score));
-
+  $file = 'score.json';
+  
+  $response = Unirest::get("https://indianapolis.bracketsforgood.org/?citimark-division");
+  
+  //PIH
+  $res = explode('p2_entry_id":158,"p2_score":"', $response->body);
+  $res = explode('","', $res[1]);
+  $score['pih'] = number_format($res[0]);
+  
+  //Other
+  $res = explode('p1_entry_id":154,"p1_score":"', $response->body);
+  $res = explode('","', $res[1]);
+  $score['other'] = number_format($res[0]);
+  
+  file_put_contents($file, json_encode($score));
+  
+  print "update. ";
+}
